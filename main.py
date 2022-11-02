63827dd9313e40235acea543b180ae1862ab2efc
jblanquicett92 <jorgeabm1992@gmail.com>
2022-11-02 14:03:39 -0600

    refactor(main): delete main trash code

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 import pyautogui
 import os
 from os import listdir
-
-
-BASE_PATH = os.getenv('BASE_PATH')
-print(BASE_PATH)
-onlyfiles = [f for f in listdir(BASE_PATH) 
-if os.path.isfile(os.path.join(BASE_PATH, f))]
-
-print(onlyfiles)
-
-for i in onlyfiles:
-    print(i, os.path.getsize(BASE_PATH+'/'+i))
