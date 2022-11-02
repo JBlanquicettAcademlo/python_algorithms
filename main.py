676a5425b99edff26c69c5b7504518556d5c9023
jblanquicett92 <jorgeabm1992@gmail.com>
2022-11-02 13:53:28 -0600

    feat(main-project): add tools to make a wonderful rpa project with a stack data structure

--- main.py
+++ main.py
@@ -1,42 +1,14 @@
+import pyautogui
+import os
+from os import listdir
 
 
+BASE_PATH = os.getenv('BASE_PATH')
+print(BASE_PATH)
+onlyfiles = [f for f in listdir(BASE_PATH) 
+if os.path.isfile(os.path.join(BASE_PATH, f))]
 
+print(onlyfiles)
 
-
-#Sebas Aguero
-# def current_speed(d,t):
-#     v= d/t 
-#     if (int(v)<=30) :
-#         return 'el automovil va lento'
-#     elif (int(v)>30 and int(v)<=70): 
-#         return 'el automovil va normal'
-#     else: 
-#         return 'el automovil va muy rápido'
-
-# print(current_speed(300,20))
-
-#Jorge Lopez
-def speed_car(distance, time):
-    v = distance * time
-
-    if (v == 2000):
-        print("Too fast")
-
-    elif (v > 1000 and v < 2000): #refactor
-        print("Normal")
-
-    else:
-        print("Too slow")
-speed_car(100, 15)
-
-#Billy Campagnoly
-def current_speed(D, T):
-    V = D/T
-    if (V >= 100):
-        print("Slow down")
-    elif (V == 80):
-        print("continue")
-    else:
-        print("Speed up")
-
-current_speed(100, 5)
+for i in onlyfiles:
+    print(i, os.path.getsize(BASE_PATH+'/'+i))
